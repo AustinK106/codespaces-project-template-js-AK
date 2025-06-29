7214d2b778e4f32de43feb9a6c245025c9073efd
AustinK106 <austinkonneh24@gmail.com>
2025-06-29 03:14:21 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27115,7 +27115,7 @@ var _stylesCss = require("./styles.css");
     email: "austinkonneh24@gmail.com",
     gitHub: "microsoft",
     instagram: "microsoft",
-    linkedIn: "austin-konneh",
+    linkedIn: "https://www.linkedin.com/in/austin-konneh-239847324/",
     medium: "",
     twitter: "microsoft",
     youTube: "Code"
